9d58e7be51387c2352b8c6b46cf8418151dc3bca
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-11-02 14:39:22 +0100

    inserito il metodo per inserire nuovi elementi

--- js/script.js
+++ js/script.js
@@ -3,14 +3,35 @@ Vue.config.devtools = true;
 new Vue({
   el: "#root",
   data: {
+    titolo:"Hello Vueoolean - Todolist",
+    coseDaFare:["prova","prova2"],
+    nuovoElemento:"",
     
   },
+  methods:{
+    aggiungiElemento(){
 
-  methods: {
-   
+      if (this.nuovoElemento.trim() === ""){
+        return;
+      }
+
+      const elementoPresente = this.coseDaFare.some((el) =>{
+        return el.toLowerCase() === this.nuovoElemento.trim().toLowerCase();
+      })
+
+      if(elementoPresente) {
+        return;
+      }
+
+      this.coseDaFare.push(this.nuovoElemento.trim());
+
+      this.nuovoElemento ="";
+
+
+    }
 
   }
- 
+
 });
 
 
